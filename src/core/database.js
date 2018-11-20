1a1d3e1a66760745e8a914e0da22424aea6037e7
Said Furkan Dize <furkan.dize@akinon.com>
2018-11-20 23:51:34 +0300

    master [REMOVED] unneccessary variables

--- src/core/database.js
+++ src/core/database.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import User from '../models/user';
 
 mongoose.connect('mongodb://localhost/magazine');
 
@@ -9,18 +8,3 @@ conn.on('error', console.error.bind(console, 'connection error:'));
 conn.once('open', () => {
   console.log('Mongoose Connected');
 });
-
-const user = new User({
-  username: 'faadssdasdadsds',
-  email: 'faasdsdasdadsds',
-  password: 'fasdasdadsds',
-  avatar: 'fasdasdadsds',
-  firstname: 'fasdasdadsds',
-  lastname: 'fasdasdadsds',
-});
-
-user.save((err, res) => {
-  if (err) { console.log('err', err)}
-  console.log(res)
-});
-
