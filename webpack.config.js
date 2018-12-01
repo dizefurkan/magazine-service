858b8cc6763df706d7d59406a628738fb30e70db
Said Furkan Dize <furkan.dize@akinon.com>
2018-12-01 15:51:27 +0300

    master [ADDED] Webpack added and babel configuration adjusted

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const nodeModules = {};
+
+fs
+  .readdirSync('node_modules')
+  .forEach(function(mod) {
+    if (mod !== '.bin') {
+      nodeModules[mod] = 'commonjs ' + mod;
+    }
+  });
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  node: {
+    fs: 'empty',
+    __dirname: true,
+    __filename: true,
+  },
+  externals: nodeModules,
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ]
+  }
+};
